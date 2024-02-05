f3749d9915ce0aee58836c6c1c0db6bc126dd691
Muhammad Daniyal Mehmood <hello.world@codeline.rihal.om>
2024-02-05 12:16:23 +0400

    Added Calculator for Integers

---
+++ IntegerCalculator.java
@@ -0,0 +1,14 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int firstNum = 0;
+        int secondNum = 8;
+      
+      System.out.println(firstNum + secondNum);
+      
+      
+       
+    }
+}
